41ab59bd9f78a068cfa80814403549e0d2fe7024
Ysreimer Ortiz <ysreimer@gmail.com>
2025-02-03 20:54:41 -0500

    errores corregidos

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -36,7 +36,7 @@ export default function useWeather() {
     setWeather(inicialState)
     try {
 
-        const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+        const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
     const {data} = await axios(geoUrl)
 
